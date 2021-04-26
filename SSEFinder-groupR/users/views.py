a14180acf45874a4410d2f2f8699bbe7a2e86820
Raymondckm <73030895+Raymondckm@users.noreply.github.com>
2021-04-26 16:14:58 +0800

    initial testing of API

--- SSEFinder-groupR/users/views.py
+++ SSEFinder-groupR/users/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from urllib.parse import quote
+import json, requests
 
 # Create your views here.
  
 def user_authentication(request):
     return HttpResponse("User authentication")
+ 
+ 
+# Geodata Store API
+venue_name = input('Enter the venue name: ')
+venue_location = input('Enter the location name: ')
+api_endpoint = 'https://geodata.gov.hk/gs/api/v1.0.0/locationSearch?q='
+querystring = quote(f'{venue_name} {venue_location}')
+#print(api_endpoint + querystring)
+response = requests.get(api_endpoint + querystring)
+#print(response)
+
+if response.status_code == 200:
+    # assumption: only 1 match
+    address = response.json()[0]['addressEN']
+    X_Coord = response.json()[0]['x']
+    Y_Coord = response.json()[0]['y']
+
+    #print(address)
+    #print(X_Coord)
+    #print(Y_Coord)
+ 
