3f0f3d373ae4aca1a984becb9afb8a4db79461f6
shreeya5 <21shreeya@gmail.com>
2021-04-29 16:04:48 +0800

    dep

--- SSEFinder-groupR/SSEFinder_config/settings.py
+++ SSEFinder-groupR/SSEFinder_config/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5_r^h&ghr&okg%o*ldck^umi5qo!wohzig()ijsg#x#yyp4ya)'
+SECRET_KEY = env('CHP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('CHP_DEBUG', default=False)
 
 ALLOWED_HOSTS = ['arcane-temple-33137.herokuapp.com/', 'localhost', '127.0.0.1']
 
