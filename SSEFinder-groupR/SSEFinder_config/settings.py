a280bd6b48dedf4a2446083512dc76be75aa122c
shreeya5 <21shreeya@gmail.com>
2021-04-29 16:30:34 +0800

    dep

--- SSEFinder-groupR/SSEFinder_config/settings.py
+++ SSEFinder-groupR/SSEFinder_config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('CHP_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('CHP_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['arcane-temple-33137.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['arcane-temple-33137.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
